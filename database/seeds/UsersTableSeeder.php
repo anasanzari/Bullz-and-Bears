e9a5688baf0392771777db99ccf965cf74d9af39
anasanzari <anasanzari@gmail.com>
2017-07-25 15:25:02 +0530

    add AppState Seed for Game initialization

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\User;
+use App\AppState;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,19 +12,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-      $users = array(
-              ['name' => 'Ryan Chenkie', 'email' => 'ryanchenkie@gmail.com', 'password' => Hash::make('secret')],
-              ['name' => 'Chris Sevilleja', 'email' => 'chris@scotch.io', 'password' => Hash::make('secret')],
-              ['name' => 'Holly Lloyd', 'email' => 'holly@scotch.io', 'password' => Hash::make('secret')],
-              ['name' => 'Adnan Kukic', 'email' => 'adnan@scotch.io', 'password' => Hash::make('secret')],
-      );
-
-      // Loop through each user above and create the record for them in the database
-      foreach ($users as $user)
-      {
-          User::create($user);
+      $app = AppState::find(1);
+      if($app){
+        echo "State Exists.\n";
+        return;
       }
-
-      Model::reguard();
+      $app = new AppState;
+      $app->id = 1;
+      $app->state = AppState::STATE_ACTIVE;
+      $app->save();
+      echo "State Created.\n";
     }
 }
