1d41a1fad94662c0487f32da6f88a3ce9ede2636
Afeedh Shaji <afeedh_b170282cs@nitc.ac.in>
2021-02-08 16:20:05 +0530

    Updated NSEIndia URL

--- app/Console/Commands/InitStocks.php
+++ app/Console/Commands/InitStocks.php
@@ -33,14 +33,15 @@ class InitStocks extends Command
         parent::__construct();
     }
 
-    private function curl_get_contents($url){
+    private function curl_get_contents($url)
+    {
 
 
-       $headers = array(
-         "Accept: application/json",
-         "Content-type: application/json",
-         "Connection: keep-alive"
-       );
+        $headers = array(
+            "Accept: application/json",
+            "Content-type: application/json",
+            "Connection: keep-alive"
+        );
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -48,11 +49,10 @@ class InitStocks extends Command
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
         $json = curl_exec($ch);
         curl_close($ch);
         return $json;
-
     }
 
     /**
@@ -60,32 +60,33 @@ class InitStocks extends Command
      *
      * @return mixed
      */
-    public function handle(){
-      //clear db
-      DB::table('stocks')->delete();
-      $res = $this->curl_get_contents("https://nseindia.com/live_market/dynaContent/live_watch/stock_watch/niftyStockWatch.json");
-      $this->info($res);
-      $jsonobj = json_decode($res);
-      $this->info('Fetch Complete: Time Stamp : '.$jsonobj->time);
-      $update_time = date('Y-m-d H:i:s', strtotime($jsonobj->time));
+    public function handle()
+    {
+        //clear db
+        DB::table('stocks')->delete();
+        $res = $this->curl_get_contents("https://www1.nseindia.com/live_market/dynaContent/live_watch/stock_watch/niftyStockWatch.json");
+        $this->info($res);
+        $jsonobj = json_decode($res);
+        $this->info('Fetch Complete: Time Stamp : ' . $jsonobj->time);
+        $update_time = date('Y-m-d H:i:s', strtotime($jsonobj->time));
 
-      foreach($jsonobj->{'data'} as $data){
+        foreach ($jsonobj->{'data'} as $data) {
 
-          if(str_replace(",", "",$data->ltP)!=0){
-              $this->info("Inserting ".$data->symbol);
-              Stock::create([
+            if (str_replace(",", "", $data->ltP) != 0) {
+                $this->info("Inserting " . $data->symbol);
+                Stock::create([
                     'symbol' => $data->symbol,
-                    'name' => $data->symbol,//full name is not provided in the json.
+                    'name' => $data->symbol, //full name is not provided in the json.
                     'time_stamp' => $update_time,
-                    'value' => str_replace(",","",$data->ltP),
-                    'change' => str_replace(",","",$data->ptsC),
-                    'daylow' => str_replace(",","",$data->low),
-                    'dayhigh' => str_replace(",","",$data->high),
-                    'weeklow' => str_replace(",","",$data->wklo),
-                    'weekhigh'=> str_replace(",","",$data->wkhi),
-                    'change_perc' => str_replace(",","",$data->per)
-              ]);
-          }
-      }
+                    'value' => str_replace(",", "", $data->ltP),
+                    'change' => str_replace(",", "", $data->ptsC),
+                    'daylow' => str_replace(",", "", $data->low),
+                    'dayhigh' => str_replace(",", "", $data->high),
+                    'weeklow' => str_replace(",", "", $data->wklo),
+                    'weekhigh' => str_replace(",", "", $data->wkhi),
+                    'change_perc' => str_replace(",", "", $data->per)
+                ]);
+            }
+        }
     }
 }
